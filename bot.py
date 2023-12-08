e598c95dc8f8df53ab04de0d5ff0ae5908a010d6
Dangerprobots <110118097+Dangerprobots@users.noreply.github.com>
2023-12-08 07:05:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,8 +10,8 @@ logging.basicConfig(level=logging.ERROR)
 User = Client(name="AcceptUser", api_id=environ.get("API_ID"), api_hash=environ.get("API_HASH"),
               session_string="AQAWgXwASfQaqtA5ciBxPU9x1p8TgHbFPgKb1UU7Yio6KHtttRhbElxeaYfKAqlyGBBvlK6fOibO19VjkTEeaXcc16sY9Mc_MV7jYHwL9xKZiTQVL99OzOptVqKo1H8NhK9DfK5ZL9B0jkKftAR3hdk6jnhIsS0m_u3_XIiZ7wdbNjQawvwqKhSllB0FTugjg_q3jKeiyJZr0Q1PJzsALkXIGVxl4Ak0ok8VxSrKd6zFW6LcqlQBTHZ0TWl1vXalAQZYplpztm2kQVLxpVfGzC-qV5FMwfaw8I6XbZ5_qd4O0V9-t_C4g_qmyszS1RDwlfHefWPma314RnyyafS8N3EDdaGYwAAAAAGN00lEAA"
 
-MAX_RETRIES = 5  # Maximum number of retries for approval
-RETRY_INTERVAL = 11  # Interval (in seconds) to wait before retrying
+#MAX_RETRIES = 5  # Maximum number of retries for approval
+#RETRY_INTERVAL = 11  # Interval (in seconds) to wait before retrying
 
 async def approve_all_join_requests(user, chat_id):
     max_users_per_run = 10000  # Maximum users to approve in one run
